f567a7cf2493fe8c08dd1c56bea0187862e4b50c
Dovydas Stankevicius <ds@gangverk.is>
2019-04-09 14:45:41 +0000

    tweaking log levels

--- clientlibrary/worker/shard-consumer.go
+++ clientlibrary/worker/shard-consumer.go
@@ -188,7 +188,7 @@ func (sc *ShardConsumer) getRecords(shard *shardStatus) error {
 					continue
 				}
 			}
-			log.Errorf("Error getting records from Kinesis that cannot be retried: %+v Request: %s", err, getRecordsArgs)
+			log.Warnf("Error getting records from Kinesis that cannot be retried: %+v Request: %s", err, getRecordsArgs)
 			return err
 		}
 
